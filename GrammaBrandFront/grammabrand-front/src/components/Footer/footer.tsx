877e685d849b870249ca72be2f71a64b9635b586
MrGrmm <gramma1997ion@gmail.com>
2024-02-03 00:08:23 +0100

    Update header & footer

--- GrammaBrandFront/grammabrand-front/src/components/Footer/footer.tsx
+++ GrammaBrandFront/grammabrand-front/src/components/Footer/footer.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import './footer.css'; // Убедитесь, что файл стилей подключен правильно
 
 const Footer = () => {
-  // Ваш JSX код для компонента
-  return <footer>Footer...</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__contact-info">
+        <p>Italia, Venice, Mestre</p>
+        <p>gramma1997ion@gmail.com</p>
+        <p>+39555459823</p>
+      </div>
+      <div className="footer__logo">
+        <img src="footer_logo_name.png" alt="ION GRAMMA" />
+      </div>
+      <div className="footer__social-links">
+        {/* Здесь должны быть иконки или текстовые ссылки на ваши социальные сети */}
+        {/* Например: */}
+        <a href="http://linkedin.com/in/yourprofile">LinkedIn</a>
+        <a href="http://twitter.com/yourprofile">Twitter</a>
+        <a href="http://facebook.com/yourprofile">Facebook</a>
+      </div>
+      <div className="footer__activation">
+        {/* Элемент для активации, если это кнопка или ссылка */}
+        <a href="/activation-link">Click to open link</a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- GrammaBrandFront/grammabrand-front/src/components/Header/header.tsx
+++ GrammaBrandFront/grammabrand-front/src/components/Header/header.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import './header.css'; // Убедитесь, что путь до файла с CSS корректный
 
 const Header = () => {
-  // Ваш JSX код
-  return <header>Header</header>;
+  return (
+    <header className="header">
+      {/* Если у вас есть логотип как изображение */}
+      <img src="header_logo_112x112.png" alt="Logo" className="header__logo-img" />
+      <div className="header__logo">ION GRAMMA</div> {/* Если логотип текстовый */}
+      {/* Если у вас текстовый логотип */}
+      {/* <div className="header__logo">ION GRAMMA</div> */}
+      
+      <nav className="header__navigation">
+        {/* Элементы навигации */}
+        <a href="/blog">Blog</a>
+        <a href="/about-me">About me</a>
+        <a href="/about-exp">About exp</a>
+        <a href="/projects">Projects</a>
+        <a href="/contact">Contact</a>
+      </nav>
+    
+    </header>
+  );
 };
 
 export default Header;
